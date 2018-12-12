2b65effcbb7b40cc139df7a342813ca937637db5
Thom Martens <thommartens14@hotmail.com>
2018-12-12 20:42:23 +0100

    fix

--- client/inventoryscribe/server.js
+++ client/inventoryscribe/server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/client/inventoryscribe'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/index.html'));
+res.sendFile(path.join(__dirname+'/dist/client/inventoryscribe/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
